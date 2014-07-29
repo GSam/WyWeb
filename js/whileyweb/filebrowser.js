ee322ee6750c13534c2ba68d057051681052b84d
Adrian Cochrane <alcinnz@eml.cc>
2014-07-30 10:12:31 +1200

    Adding ability to create, rename, etc files & folders.

--- js/whileyweb/filebrowser.js
+++ js/whileyweb/filebrowser.js
@@ -13,6 +13,46 @@ $(function() {
                 li_attr: {},
                 a_attr: {}
             }
-        ]
+        ],
+    	core: {check_callback: true},
+    	plugins: ["contextmenu", "dnd", "types", "unique"],
+    	contextmenu: {
+    		items: function(node) {
+                var tmp = $.jstree.defaults.contextmenu.items()
+                delete tmp.create.action;
+                tmp.create.submenu = {
+                    create_folder: {
+                        label: "Folder",
+                        action: function(data) {
+                            var inst = $.jstree.reference(data.reference),
+                                obj = inst.get_node(data.reference);
+                            inst.create_node(obj, {type: "default", text: "New folder"}, "last", function(new_node) {
+                                setTimeout(function() {inst.edit(new_node);}, 0);
+                            });
+                        }
+                    },
+                    create_file: {
+                        label: "File",
+                        action: function(data) {
+                            var inst = $.jstree.reference(data.reference),
+                                obj = inst.get_node(data.reference);
+                            inst.create_node(obj, {type: "file", text: "New File"}, "last", function(new_node) {
+                                setTimeout(function() {inst.edit(new_node); }, 0);
+                            })
+                        }
+                    }
+                }
+                if (this.get_type(node) == "file")
+                    delete tmp.create;
+                return tmp;
+            }
+        },
+        types: {
+            "default": {},
+            "file": {
+                max_children: 0,
+                icon: "/images/wylogo_small.png"
+            }
+        }
     })
 });
