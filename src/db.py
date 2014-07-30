55e07444dda64a83fc7f73b7a3a04c63706b6267
Iury Painelli <irpainel@gmail.com>
2014-07-30 16:57:03 +1200

    login form, verification and session

--- src/db.py
+++ src/db.py
@@ -2,12 +2,17 @@ import cherrypy
 
 import mysql.connector
 
-#cnx = cursor = None
+#connection details
 def connect(): 
-	#global cnx
-	#global cursor
-	cnx = mysql.connector.connect(user='whiley', password='coyote',host='kipp-cafe.ecs.vuw.ac.nz',database='whiley') 
-
-	return cnx
+    try:
+        cnx = mysql.connector.connect(user='whiley', password='coyote',host='kipp-cafe.ecs.vuw.ac.nz',database='whiley')
+    except mysql.connector.Error as err:
+        if err.errno == errorcode.ER_ACCESS_DENIED_ERROR:
+            status = "Something is wrong with your user name or password"
+        elif err.errno == errorcode.ER_BAD_DB_ERROR:
+            status = "Database does not exists"
+        else:
+            status = err
+    return cnx
 
 
