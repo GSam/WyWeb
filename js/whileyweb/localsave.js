aec3c8a785dd36d28621b9b0c69a3985b6b98e4c
Garming <garming_sam@outlook.com>
2014-10-01 10:59:55 +1300

    Add some form of local storage for logged in users

--- js/whileyweb/localsave.js
+++ js/whileyweb/localsave.js
@@ -1,6 +1,12 @@
 function getFileData() {
-    if ("files" in localStorage && !isLoggedIn)
+    if ("files" in localStorage && !isLoggedIn) {
         return JSON.parse(localStorage["files"])
+    }
+
+    if (isLoggedIn && loggedStorage != undefined) {
+        return loggedStorage;
+    }
+    
     return serverFiles
 }
 
@@ -9,5 +15,8 @@ function saveFile() {
         var $files = $('#file-browser');
     
         localStorage["files"] = JSON.stringify($files.jstree(true).get_json('#', {'flat':true}));
+    } else {
+        var $files = $('#file-browser');
+        loggedStorage = JSON.stringify($files.jstree(true).get_json('#', {'flat':true}));
     }
 }
